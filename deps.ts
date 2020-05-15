73a9c2d1a51d2be6f4b4242c2642a5efe2b03390
Alexander Paul <alexanderpaul.marinas@gmail.com>
2020-05-15 18:22:58 +0800

    Add verstion tags on dependencies

--- deps.ts
+++ deps.ts
@@ -1,10 +1,9 @@
 // http
-export { serve } from 'https://deno.land/std/http/server.ts';
-export { Server, ServerRequest } from 'https://deno.land/std/http/server.ts';
+export { serve, Server, ServerRequest } from 'https://deno.land/std@v0.51.0/http/server.ts';
 
 // fs
-export { readFileStr } from 'https://deno.land/std/fs/read_file_str.ts';
-export { exists } from 'https://deno.land/std/fs/exists.ts';
+export { readFileStr } from 'https://deno.land/std@v0.51.0/fs/read_file_str.ts';
+export { exists } from 'https://deno.land/std@v0.51.0/fs/exists.ts';
 
 // path
-export * as path from 'https://deno.land/std/path/mod.ts';
+export * as path from 'https://deno.land/std@v0.51.0/path/mod.ts';
